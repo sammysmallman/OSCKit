e7d29b2f10995df082969ef5d0634e5dc2f5634d
Sam Smallman <srsmallman@mac.com>
2021-04-20 09:14:33 +0100

    Closes #30
    Added minimum platform requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "OSCKit",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
